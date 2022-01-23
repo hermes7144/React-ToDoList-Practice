9b1775556ac3b5b948f28bc986588de9eacfc02b
hermes7144 <kichancho@gmail.com>
2022-01-23 09:56:53 +0900

    Todo 템플릿까지 완료

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
+import React, { useState } from "react";
+
 import FilterButton from './components/FilterButton';
-import Todo from './components/Todo'
+import Todo from './components/Todo';
 import Form from './components/Form';
+import { nanoid } from "nanoid";
+
 function App(props) {
-  const taskList = props.tasks.map(task => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map(task => (
     <Todo 
-      id={task.Id}
-      name={task.Name}
+      id={task.id}
+      name={task.name}
       completed={task.completed}
       key={task.Id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
 
+  function toggleTaskCompleted(id) {
+    // if this task has the same ID as the edited task
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function addTask(name){
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask])
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask (id, newName) {
+    const editTaskList = tasks.map(task => {
+      if (id === task.id){
+        return {...task, name: newName}
+        }
+        return task;
+    });
+    setTasks(editTaskList)
+  }
+  const taskNoun = tasks.length > 1? 'tasks' : 'task';
+  const headingText = `${tasks.length} ${taskNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         className="todo-list stack-large stack-exception"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,25 @@
+import React, { useState } from "react";
+
 function Form(props) {
+    const [name, setName] = useState('');
+
+
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (name !== "") {
+            props.addTask(name);
+            setName('');
+        } else {
+            alert("기입을 해주세요")
+        }
+        
+    }
+    
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
             <label htmlFor="new-todo-input" className="label__lg">
                 What needs to be done?
@@ -10,8 +29,9 @@ function Form(props) {
             type="text"
             id="new-todo-input"
             className="input input__lg"
-            name="text"
+            value={name}
             autoComplete="off"
+            onChange={handleChange}
             />
             <button type="submit" className="btn btn__primary btn__lg">
             Add
