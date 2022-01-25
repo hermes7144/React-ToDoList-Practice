930766b0a331817026b55092c58c6ea8a51bb962
hermes7144 <kichancho@gmail.com>
2022-01-25 14:55:28 +0900

    Todo 템플릿까지 완료-2

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { useState, useRef, useEffect } from "react";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import { nanoid } from "nanoid";
+import usePrevious from "./functions";
+
+const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+    const [tasks, setTasks] = useState(props.tasks);
+    const [filter, setFilter] = useState('All');
+    const listHeadingRef = useRef(null);
+
+    const filterList = FILTER_NAMES.map(name =>
+        <FilterButton 
+            key={name} 
+            name={name}
+            ispressed={name === filter}
+            setFilter={setFilter}
+        />
+    );
+
+    const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task =>
+        <Todo 
+            id={task.id} 
+            name={task.name} 
+            completed={task.completed}
+            key={task.id}
+            toggleTaskCompleted={toggleTaskCompleted}
+            editTask={editTask}
+            deleteTask={deleteTask}
+        />
+        );
+    
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map(task =>{
+            if (id === task.id){
+                return {...task, completed: !task.completed}
+            }
+            return task;
+        })
+        setTasks(updatedTasks);
+    }
+    
+    function addTask(name) {
+        const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+        setTasks([...tasks, newTask]);
+    }
+
+    function deleteTask(id) {
+        const remainingTasks = tasks.filter(task => id !== task.id);
+        setTasks(remainingTasks)
+    }
+
+    function editTask(id, newName){
+        const editTaskList = tasks.map(task => {
+        // if this task has the same ID as the edited task
+        if (id === task.id) {
+            return {...task, name: newName}
+        }
+        return task;
+        })
+        setTasks(editTaskList);
+    }
+
+    const prevTaskLength = usePrevious(tasks.length);
+
+    useEffect(() => {
+        if (tasks.length - prevTaskLength === -1) {
+            listHeadingRef.current.focus();
+        }
+    }, [tasks.length, prevTaskLength]);
+
+    const tasksNoun = tasks.length > 1 ? "tasks" : "task";
+    const headingText = `${tasks.length} ${tasksNoun} remaining`;
+    return (
+        <div className="todoapp stack-large">
+            <h1>TodoMatic</h1>
+            <Form addTask={addTask}/>
+            <div className="filters btn-group stack-exception">
+                {filterList}
+            </div>
+            <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+                {headingText}
+            </h2>
+            <ul
+            className="todo-list stack-large stack-exception"
+            aria-labelledby="list-heading"
+            >
+                {taskList}
+            </ul>
+        </div>
+    );
+  }
+
+export default App;
